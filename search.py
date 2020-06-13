84ddf9f35d3688f1dcc927b9b90a197495b80745
sarikhin <37961417+IKHINtech@users.noreply.github.com>
2020-06-13 11:34:39 -0700

    Create search.py

---
+++ search.py
@@ -0,0 +1,48 @@
+import tweepy,sys,jsonpickle
+
+consumer_key = '5iYbkEKrAireNEifqkNOsrhGw'
+consumer_secret = 'bj1ympGz2wzftntNqRGb4EMPAU4Eg6PlfdTRlgDYxSrUhXOOO8'
+print("#===============================================#")
+print("Program Crawling Twitter menggunakan Python 3")
+print("\t SARIKHIN TI.17.D.7")
+print("\t PELITA BANGSA")
+print("#===============================================#")
+
+
+qry = input("Masukkan Query yang akan anda cari :") #input query yang akan dicari
+fName = input("Nama File Hasil Crawling :") #input nama file hasil pencarian
+maxTweets = input("Isi Maksimal Tweets :") #500# Isi sembarang nilai sesuai kebutuhan anda
+tweetsPerQry = 100  # Jangan isi lebih dari 100, ndak boleh oleh Twitter
+
+auth = tweepy.AppAuthHandler(consumer_key,consumer_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+if (not api):
+    sys.exit('Autentikasi gagal, mohon cek "Consumer Key" & "Consumer Secret" Twitter anda')
+
+sinceId, max_id, tweetCount = None, -1, 0
+
+print("Mulai mengunduh maksimum {0} tweets".format(maxTweets))
+with open(fName,'w') as f:
+    while tweetCount < maxTweets:
+        try:
+            if (max_id <= 0):
+                if (not sinceId):
+                    new_tweets=api.search(q=qry,count=tweetsPerQry)
+                else:
+                    new_tweets=api.search(q=qry,count=tweetsPerQry,since_id=sinceId)
+            else:
+                if (not sinceId):
+                    new_tweets=api.search(q=qry,count=tweetsPerQry,max_id=str(max_id - 1))
+                else:
+                    new_tweets=api.search(q=qry,count=tweetsPerQry,max_id=str(max_id - 1),since_id=sinceId)
+            if not new_tweets:
+                print('Tidak ada lagi Tweet ditemukan dengan Query="{0}"'.format(qry));break
+            for tweet in new_tweets:
+                f.write(jsonpickle.encode(tweet._json,unpicklable=False)+'\n')
+            tweetCount+=len(new_tweets)
+            sys.stdout.write("\r");sys.stdout.write("Jumlah Tweets telah tersimpan: %.0f" %tweetCount);sys.stdout.flush()
+            max_id=new_tweets[-1].id
+        except tweepy.TweepError as e:
+            print("some error : " + str(e));break # Aya error, keluar
+print ('\nSelesai! {0} tweets tersimpan di "{1}"'.format(tweetCount,fName))
